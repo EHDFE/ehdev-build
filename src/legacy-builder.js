b9ec2f8557e2a93eb6790428c97445f805875169
杰枫 <huanggbxjp@sohu.com>
2017-11-29 10:41:01 +0800

    ADD: sub dir compress support

--- src/legacy-builder.js
+++ src/legacy-builder.js
@@ -1,22 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const stream = require('stream');
-const chalk = require('chalk');
-const inquirer = require('inquirer');
-const gulp = require('gulp');
-const uglifyjs = require('uglify-es');
-const composer = require('gulp-uglify/composer');
-const cleanCSS = require('gulp-clean-css');
-const logger = require('gulp-logger');
-const gulpif = require('gulp-if');
-const Rx = require('rx-lite');
-const pump = require('pump');
+const fs = require("fs");
+const path = require("path");
+const stream = require("stream");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const gulp = require("gulp");
+const uglifyjs = require("uglify-es");
+const composer = require("gulp-uglify/composer");
+const cleanCSS = require("gulp-clean-css");
+const logger = require("gulp-logger");
+const gulpif = require("gulp-if");
+const Rx = require("rx-lite");
+const pump = require("pump");
 
-
-const getSubdirChoices = require('./getSubdirChoices');
+const getSubdirChoices = require("./getSubdirChoices");
 
 const makePrompt = ({ name, message, choices }) => ({
-  type: 'list',
+  type: "list",
   name,
   message,
   default: choices[0],
@@ -36,77 +35,101 @@ module.exports = (options, projectConfig) => {
   const iq = inquirer.prompt(prompts);
   const cacheAnswer = {};
   iq.ui.process.subscribe(
-    ({ name, answer }) => { 
+    ({ name, answer }) => {
       Object.assign(cacheAnswer, {
         [name]: answer,
       });
       switch (name) {
-        case 'project':
+        case "project":
           fs.readdir(path.resolve(cwd, answer), (err, data) => {
-            const subDir = data.filter(d => !d.startsWith('.'));
-            prompts.onNext(makePrompt({
-              name: 'branch',
-              message: `Please select the ${chalk.yellow('directory')} or branch to compile`,
-              choices: subDir,
-            }));
+            const subDir = data.filter(d => !d.startsWith("."));
+            prompts.onNext(
+              makePrompt({
+                name: "branch",
+                message: `Please select the ${chalk.yellow(
+                  "directory"
+                )} or branch to compile`,
+                choices: subDir,
+              })
+            );
           });
           break;
-        case 'branch':
-          const confirmDirectory = path.resolve(cwd, `${cacheAnswer.project}/${cacheAnswer.branch}`);
+        case "branch":
+          const confirmDirectory = path.resolve(
+            cwd,
+            `${cacheAnswer.project}/${cacheAnswer.branch}`
+          );
           prompts.onNext({
-            type: 'confirm',
-            name: 'directory',
-            message: `Is this the exactly ${chalk.yellow('directory')} (${chalk.green(confirmDirectory)}) you wanna to compile`,
+            type: "confirm",
+            name: "directory",
+            message: `Is this the exactly ${chalk.yellow(
+              "directory"
+            )} (${chalk.green(confirmDirectory)}) you wanna to compile`,
             default: false,
           });
           break;
-        case 'directory':
+        case "directory":
           if (!cacheAnswer.directory) return prompts.onCompleted();
-          const projectDirectory = path.resolve(cwd, `${cacheAnswer.project}/${cacheAnswer.branch}`);
+          const projectDirectory = path.resolve(
+            cwd,
+            `${cacheAnswer.project}/${cacheAnswer.branch}`
+          );
           fs.readdir(projectDirectory, (err, data) => {
             const subDir = data.filter(d => {
-              return d !== 'dist' && !d.startsWith('.') && fs.statSync(path.join(projectDirectory, d)).isDirectory();
+              return (
+                d !== "dist" &&
+                !d.startsWith(".") &&
+                fs.statSync(path.join(projectDirectory, d)).isDirectory()
+              );
             });
             prompts.onNext({
-              type: 'checkbox',
-              name: 'minifyDirs',
-              message: 'Please select the dirs that you want to apply compressing.',
-              default: subDir.filter(d => d.endsWith('Modules')),
+              type: "checkbox",
+              name: "minifyDirs",
+              message:
+                "Please select the dirs that you want to apply compressing.",
+              default: subDir.filter(d => d.endsWith("Modules")),
               choices: subDir,
             });
           });
           break;
-        case 'minifyDirs':
-          let hasModules = answer.filter((d) => d.endsWith('Modules'));
-          if(!hasModules.length){
+        case "minifyDirs":
+          let hasModules = answer.filter(d => d.endsWith("Modules"));
+          if (!hasModules.length) {
             prompts.onNext({
-              type: 'list',
-              name: 'uglyStrategy',
-              message: '未选中文件处理策略',
-              default: '不做处理',
-              choices: ['不做处理', '直接复制'],
+              type: "list",
+              name: "uglyStrategy",
+              message: "未选中文件处理策略",
+              default: "不做处理",
+              choices: ["不做处理", "直接复制"],
             });
-          }else{
-            const { defaultChoices, choices } = getSubdirChoices(path.resolve(cwd, `${cacheAnswer.project}/${cacheAnswer.branch}`, hasModules[0]));
+          } else {
+            const { defaultChoices, choices } = getSubdirChoices(
+              path.resolve(
+                cwd,
+                `${cacheAnswer.project}/${cacheAnswer.branch}`,
+                hasModules[0]
+              )
+            );
             prompts.onNext({
-              type: 'checkbox',
-              name: 'subDirs',
-              message: 'Please select the  sub dirs that you want to apply compressing.',
+              type: "checkbox",
+              name: "subDirs",
+              message:
+                "Please select the  sub dirs that you want to apply compressing.",
               default: defaultChoices,
-              choices: choices.concat(['All']),
+              choices: choices.concat(["All"]),
             });
           }
           break;
-        case 'subDirs':
+        case "subDirs":
           prompts.onNext({
-            type: 'list',
-            name: 'uglyStrategy',
-            message: '未选中文件处理策略',
-            default: '不做处理',
-            choices: ['不做处理', '直接复制'],
+            type: "list",
+            name: "uglyStrategy",
+            message: "未选中文件处理策略",
+            default: "不做处理",
+            choices: ["不做处理", "直接复制"],
           });
           break;
-        case 'uglyStrategy':
+        case "uglyStrategy":
           prompts.onCompleted();
           break;
         default:
@@ -114,62 +137,97 @@ module.exports = (options, projectConfig) => {
           break;
       }
     },
-    (err) => {
+    err => {
       console.log(chalk.red(err));
     }
   );
 
-  prompts.onNext(makePrompt({
-    name: 'project',
-    message: 'Please select the project to compile',
-    choices: projectList,
-  }));
+  prompts.onNext(
+    makePrompt({
+      name: "project",
+      message: "Please select the project to compile",
+      choices: projectList,
+    })
+  );
 
   iq.then(answers => {
     if (answers.directory) {
       const rootPath = `${answers.project}/${answers.branch}`;
       const minifyDirs = answers.minifyDirs;
-      let initArr;
-      if(answers.uglyStrategy !== '不做处理'){
-        initArr = [`${rootPath}/**/*`, `!${rootPath}/dist/**/*`];
+      let hasModules = minifyDirs.filter(d => d.endsWith("Modules")).length;
+      let subDirs = answers.subDirs;
+      let fileSrc;
+      let initArr = [];
+      let minifyJS = [];
+      let minifyCSS = [];
+      if (answers.uglyStrategy === "不做处理") {
+        initArr = [`${rootPath}/*`];
       } else {
-        initArr = minifyDirs.map((d)=> {
-          return `${rootPath}/${d}/**/*`;
-        }).concat([
-          `${rootPath}/*`,
-        ]);
+        initArr = [`${rootPath}/**/*`, `!${rootPath}/dist/**/*`];
       }
-      pump([
-        gulp.src(
-          minifyDirs.reduce((prev, dir) => prev.concat([
+
+      if (hasModules && subDirs.indexOf("All") === -1) {
+        fileSrc = minifyDirs.reduce((prev, dir) => {
+          if (dir.endsWith("Modules")) {
+            return prev.concat(
+              subDirs.reduce((subPrev, subDir) => {
+                minifyJS.push(`${rootPath}/${dir}/${subDir}/**/*.js`);
+                minifyCSS.push(`${rootPath}/${dir}/${subDir}/**/*.css`);
+                return subPrev.concat([
+                  `${rootPath}/${dir}/${subDir}/**/*`,
+                  `!${rootPath}/${dir}/${subDir}/**/*.js`,
+                  `!${rootPath}/${dir}/${subDir}/**/*.css`,
+                ]);
+              }, [])
+            );
+          } else {
+            minifyJS.push(`${rootPath}/${dir}/**/*.js`);
+            minifyCSS.push(`${rootPath}/${dir}/**/*.css`);
+            return prev.concat([
+              `${rootPath}/${dir}/**/*`,
+              `!${rootPath}/${dir}/**/*.js`,
+              `!${rootPath}/${dir}/**/*.css`,
+            ]);
+          }
+        }, initArr);
+      } else {
+        fileSrc = minifyDirs.reduce((prev, dir) => {
+          minifyJS.push(`${rootPath}/${dir}/**/*.js`);
+          minifyCSS.push(`${rootPath}/${dir}/**/*.css`);
+          return prev.concat([
+            `${rootPath}/${dir}/**/*`,
             `!${rootPath}/${dir}/**/*.js`,
             `!${rootPath}/${dir}/**/*.css`,
-          ]), initArr),
-          {
-            base: rootPath 
-          }
-        ),
+          ]);
+        }, initArr);
+      }
+      pump([
+        gulp.src(fileSrc, {
+          base: rootPath,
+        }),
         logger({
-          before: 'Starting copy assets...',
-          after: 'Copy complete!',
+          before: "Starting copy assets...",
+          after: "Copy complete!",
           showChange: true,
         }),
-        gulp.dest('./dist', {
+        gulp.dest("./dist", {
           cwd: path.resolve(cwd, `${rootPath}`),
         }),
       ]);
- 
-      minifyDirs.forEach(function(dir){
-        pump([
-          gulp.src([`${rootPath}/${dir}/**/*.js`]),
+
+      pump(
+        [
+          gulp.src(minifyJS, {
+            base: rootPath,
+          }),
           logger({
-            before: 'Starting uglify scripts...',
-            after: 'Uglify complete!',
+            before: "Starting uglify scripts...",
+            after: "Uglify complete!",
             showChange: true,
           }),
           gulpif(
-            (file) => {
-              if (path.basename(file.path, '.js').includes('.min')) {
+            file => {
+              if (path.basename(file.path, ".js").includes(".min")) {
                 return false;
               }
               return true;
@@ -184,26 +242,29 @@ module.exports = (options, projectConfig) => {
               ie8: false,
             })
           ),
-          gulp.dest(`./dist/${dir}`, {
+          gulp.dest(`./dist`, {
             cwd: path.resolve(cwd, `${rootPath}`),
           }),
-        ], function(err){
+        ],
+        function(err) {
           if (err) {
             console.log(chalk.red(err));
           }
-        });
-      });
-      minifyDirs.forEach(function(dir){
-        pump([
-          gulp.src(`${rootPath}/${dir}/**/*.css`),
+        }
+      );
+      pump(
+        [
+          gulp.src(minifyCSS, {
+            base: rootPath,
+          }),
           logger({
-            before: 'Starting cleancss...',
-            after: 'Cleancss complete!',
+            before: "Starting cleancss...",
+            after: "Cleancss complete!",
             showChange: true,
           }),
           gulpif(
-            (file) => {
-              if (path.basename(file.path, '.css').includes('.min')) {
+            file => {
+              if (path.basename(file.path, ".css").includes(".min")) {
                 return false;
               }
               return true;
@@ -212,25 +273,25 @@ module.exports = (options, projectConfig) => {
               rebase: false,
             })
           ),
-          gulp.dest(`./dist/${dir}`, {
+          gulp.dest(`./dist`, {
             cwd: path.resolve(cwd, `${rootPath}`),
           }),
-        ], function(err){
+        ],
+        function(err) {
           if (err) {
             console.log(chalk.red(err));
           }
-        });
+        }
+      );
+
+      const versionContent = `var VERSION = ${new Date().getTime()};`;
+      fs.writeFile(`${rootPath}/version.js`, versionContent, err => {
+        if (err) console.log(err);
       });
 
-        const versionContent = `var VERSION = ${new Date().getTime()};`;
-        fs.writeFile(`${rootPath}/version.js`, versionContent,(err) => {
-          if(err) console.log(err);
-        });
-
-        console.log(chalk.green('Build Success!'));
+      console.log(chalk.green("Build Success!"));
     } else {
       process.exit(-1);
     }
   });
-  
 };
